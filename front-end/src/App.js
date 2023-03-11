2ea6eda01aea566b61b955535c76cdc5693cfa63
Dheeraj Devaraj <dheeraj020602@gmail.com>
2023-03-11 15:53:16 +0530

    Added Image Card Component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Learn React</div>;
+  return <div className="App"></div>;
 }
 
 export default App;
-xa
---
+++ front-end/src/components/Password/ImageCard.jsx
@@ -0,0 +1,7 @@
+import "./ImageCard.css";
+
+function ImageCard() {
+  return <div className="image-card">Learn React</div>;
+}
+
+export default ImageCard;
