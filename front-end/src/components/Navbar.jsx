f449ae9c6a57ffe566138ae436423689ed7c227d
Dheeraj Devaraj <dheeraj020602@gmail.com>
2023-03-11 17:30:25 +0530

    Changed image card

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,22 +1,21 @@
-import '../styles/Navbar.css'
+import "../styles/Navbar.css";
 
 const Navbar = () => {
-    return (
-        <div className="navbar-container">
-            <div className="navbar-outer-flex-item-left">
-                <div className="navbar-logo">Encrypt</div>
-            </div>
-            <div className="navbar-flex-container navbar-outer-flex-item-center">
-                <div className="navbar-flex-item  navbar-link">Encrypt</div>
-                <div className="navbar-flex-item  navbar-link">Encrypt</div>
-                <div className="navbar-flex-item  navbar-link">Encrypt</div>
-            </div>
-            <div className="navbar-flex-container navbar-outer-flex-item-right">
-                <div className="navbar-flex-item navbar-logo">Encrypt</div>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="navbar-container">
+      <div className="navbar-outer-flex-item-left">
+        <div className="navbar-logo">Encrypt</div>
+      </div>
+      <div className="navbar-flex-container navbar-outer-flex-item-center">
+        <div className="navbar-flex-item  navbar-link">Encrypt</div>
+        <div className="navbar-flex-item  navbar-link">Encrypt</div>
+        <div className="navbar-flex-item  navbar-link">Encrypt</div>
+      </div>
+      <div className="navbar-flex-container navbar-outer-flex-item-right">
+        <div className="navbar-flex-item navbar-logo">Encrypt</div>
+      </div>
+    </div>
+  );
+};
 
-
-export default Navbar
+export default Navbar;
--- front-end/src/components/Password/ImageGrid.jsx
+++ front-end/src/components/Password/ImageGrid.jsx
@@ -1,25 +1,31 @@
-import '../../styles/ImageGrid.css';
-import ImageCard from './ImageCard';
-import logo from './logo.svg';
-import axios from 'axios';
-
+import "../../styles/ImageGrid.css";
+import ImageCard from "./ImageCard";
+import logo from "./logo.svg";
+// import axios from 'axios';
 export default function ImageGrid() {
+  let images = [
+    { imgsrc: logo, selected: true, number: "3" },
+    { imgsrc: logo, selected: true, number: "3" },
+    { imgsrc: logo, selected: true, number: "3" },
+    { imgsrc: logo, selected: true, number: "3" },
+    { imgsrc: logo, selected: true, number: "3" },
+  ];
 
-    let images = [{ imgsrc: logo, selected: true, number: "3" }, { imgsrc: logo, selected: true, number: "3" }, { imgsrc: logo, selected: true, number: "3" }, { imgsrc: logo, selected: true, number: "3" }, { imgsrc: logo, selected: true, number: "3" }]
-
-
-
-    return (
-        <div className="imageGrid-container black-color-bg">
-            <div className="grid-flex-container">
-
-                {images.map((image) => {
-                    return (<div className="flex-item">
-                        <ImageCard imgsrc={image.logo} selected={image.selected} number={image.number} />
-                    </div>)
-                })}
+  return (
+    <div className="imageGrid-container black-color-bg">
+      <div className="grid-flex-container">
+        {images.map((image) => {
+          return (
+            <div className="flex-item">
+              <ImageCard
+                imgsrc={image.logo}
+                selected={image.selected}
+                number={image.number}
+              />
             </div>
-
-        </div >
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 }
