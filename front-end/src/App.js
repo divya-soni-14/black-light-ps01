9f23026abe0d97e9c476014c2f06b88c04dbda51
Dheeraj Devaraj <dheeraj020602@gmail.com>
2023-03-11 16:12:23 +0530

    Making Image Card

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import ImageCard from "./components/Password/ImageCard.jsx";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ImageCard />
+    </div>
+  );
 }
 
 export default App;
--- front-end/src/components/Password/ImageCard.jsx
+++ front-end/src/components/Password/ImageCard.jsx
@@ -1,7 +1,12 @@
 import "./ImageCard.css";
+import logo from "./nature.webp";
 
-function ImageCard() {
-  return <div className="image-card">Learn React</div>;
+function ImageCard(props) {
+  return (
+    <div className={`image-card ${props.selected}`}>
+      <img src={logo} alt="" />
+    </div>
+  );
 }
 
 export default ImageCard;
