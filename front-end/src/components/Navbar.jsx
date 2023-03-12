76da93af10509fc09e62966d8b64e7d64f90ec57
Divya <ds952073@student.nitw.ac.in>
2023-03-12 08:23:41 +0530

    further changes

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,23 +1,14 @@
 import "../styles/Navbar.css";
-import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
-        <div style={{
-            marginBottom: "5rem",
-
-        }} className="navbar-container">
+        <div className="navbar-container">
             <div className="navbar-outer-flex-item-left">
                 <div className="navbar-logo">Encrypt</div>
             </div>
-            <div className="navbar-flex-container navbar-outer-flex-item-center">
-                <div className="navbar-flex-item  navbar-link">
-                    <Link to='/login'> Login</Link>
-                </div>
-                <div className="navbar-flex-item  navbar-link"> <Link to='/'> Home</Link></div>
-                <div className="navbar-flex-item  navbar-link"> <Link to='/signup'> Signup</Link></div>
-            </div>
-            <div className="navbar-flex-container navbar-outer-flex-item-right">
-                <div className="navbar-flex-item navbar-logo">Get Help</div>
+            <div className="navbar-outer-flex-item-right">
+                <div className="navbar-link">Sign Up</div>
+                <div className="navbar-link">Log In</div>
             </div>
         </div>
     );
